b486ca986f5f79e4917cd8767830ba8271473012
DahyeKim <118429431+dahyyye@users.noreply.github.com>
2026-02-26 15:41:37 +0900

    imgui 수정

--- WindowProperty.cpp
+++ WindowProperty.cpp
@@ -5,36 +5,11 @@ void OpenProperty();
 
 void ShowWindowModelProperty(bool* p_open)
 {
-	// 윈도우 플래그(window flag)를 설정한다.
-	static bool no_titlebar = false;
-	static bool no_scrollbar = false;
-	static bool no_menu = true;
-	static bool no_move = false;
-	static bool no_resize = false;
-	static bool no_collapse = false;
-	static bool no_close = true;
-	static bool no_nav = false;
-	static bool no_background = false;
-	static bool no_bring_to_front = false;
-	static bool no_docking = false;
-	static bool unsaved_document = false;
 
 	ImGuiWindowFlags window_flags = 0;
-	if (no_titlebar)        window_flags |= ImGuiWindowFlags_NoTitleBar;
-	if (no_scrollbar)       window_flags |= ImGuiWindowFlags_NoScrollbar;
-	if (!no_menu)           window_flags |= ImGuiWindowFlags_MenuBar;
-	if (no_move)            window_flags |= ImGuiWindowFlags_NoMove;
-	if (no_resize)          window_flags |= ImGuiWindowFlags_NoResize;
-	if (no_collapse)        window_flags |= ImGuiWindowFlags_NoCollapse;
-	if (no_nav)             window_flags |= ImGuiWindowFlags_NoNav;
-	if (no_background)      window_flags |= ImGuiWindowFlags_NoBackground;
-	if (no_bring_to_front)  window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus;
-	if (no_docking)         window_flags |= ImGuiWindowFlags_NoDocking;
-	if (unsaved_document)   window_flags |= ImGuiWindowFlags_UnsavedDocument;
-	if (no_close)           p_open = NULL; // Don't pass our bool* to Begin
 
 	// 속성 윈도우를 생성하고, collapsed된 상태라면 바로 리턴한다.
-	if (!ImGui::Begin("Property", p_open, window_flags))
+	if (!ImGui::Begin("Property", nullptr, window_flags))
 	{
 		ImGui::End();
 		return;
